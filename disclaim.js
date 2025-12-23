92f484d3243009dde1154cfb66c1225a8168c2e8
isa-yan <isabel.yyj.yan@gmail.com>
2025-12-23 23:02:12 +0800

    Add files via upload

---
+++ disclaim.js
@@ -0,0 +1,13 @@
+function warningabtme(){
+    return alert("Right here is basically me ranting on and on about stuff on the plane bc I cannot use slack. it's not good so read to your own discretion");
+        }
+warningabtme();
+
+
+var urname = prompt("Your name? Stranger?");
+
+if (urname =="isa"){
+    alert("don't mess with me")
+}else{
+    alert("cool")
+}
---
+++ game.js
@@ -0,0 +1,31 @@
+function quiz(){
+    var numbery = prompt("guess my favorite number :3");
+    if (numbery==24){
+        alert("that was... that was just good luck... baka *anime tsundere blushing*");
+    }
+    else{
+        alert("You...you baka, how could you... Just because I never told you or anyone or even ChatGPT and it's something monster math forced on me *sobs in tsundere*");
+    }
+    
+    var colory = prompt("guess my fav color :3");
+    if (colory==="blue"){
+        alert("good job kiddo *pats u on head.* (IDGAF HOW OLD YOU ARE EVERYONE'S A KID. even if you're an adult, remember to take a break and foster that childlike wonder. God knows how many adults are just little kids in big people clothes doing big people thigns.");
+
+    }
+    else{
+        alert("kiddo... you could do better *frowns slightly*")
+    }
+
+    var agey = prompt("guess my age :3");
+    if(agey==14){
+        alert("age is just a number");
+    }
+    else{
+        alert("age isn't just a number");
+    }
+
+    alert('thank you for participating in my little... game')
+
+}
+
+quiz();
